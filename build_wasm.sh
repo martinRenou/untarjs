f2e87b9b57e5a149aaadfe8be8f80ddf70c1816e
martinRenou <martin.renou@gmail.com>
2026-02-03 16:38:19 +0100

    Reduce initial memory allocated for the WASM (#52)

--- build_wasm.sh
+++ build_wasm.sh
@@ -85,7 +85,7 @@ emcc unpack.c -o $WASM_LIB/unpack.js \
     ${PREFIX}/lib/libarchive.a \
     ${PREFIX}/lib/libz.a ${PREFIX}/lib/libbz2.a ${PREFIX}/lib/libzstd.a ${PREFIX}/lib/libiconv.a\
     -s MODULARIZE=1 -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
-    -s INITIAL_MEMORY=128MB \
+    -s INITIAL_MEMORY=16MB \
     -s ENVIRONMENT=web \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "getValue", "UTF8ToString", "wasmMemory"]' \
     -s EXPORTED_FUNCTIONS="['_extract', '_free_extracted_archive', '_malloc', '_free']"
